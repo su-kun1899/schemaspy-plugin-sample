51387037da79fe56abbd8f9614c4dea51d7d979a
su-kun1899 <higedrum.coz@gmail.com>
2016-08-23 19:23:14 +0900

    masterのデプロイ設定に変更

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,1 +1,5 @@
-echo $CIRCLE_BRANCH
+git config --global user.email ${GITHUB_USER_EMAIL}
+git config --global user.name ${GITHUB_USER_NAME}
+git add -A
+git commit ./docs/* -m "Update schemaSpy html [ci skip]"
+git push git@github.com:su-kun1899/schemaspy-plugin-sample.git $CIRCLE_BRANCH
