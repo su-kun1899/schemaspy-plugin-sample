f4c7a0c3cc3c6f9a0357aa6ead35c4baa4c8cd1d
su-kun1899 <higedrum.coz@gmail.com>
2016-11-02 20:09:47 +0900

    Add publishers

---
+++ src/main/resources/db/migration/V1_3_1__add_publishers.sql
@@ -0,0 +1,3 @@
+INSERT INTO publisher (publisherid, name) VALUES (1, 'ふーたく');
+INSERT INTO publisher (publisherid, name) VALUES (2, 'ひさっしゅ');
+INSERT INTO publisher (publisherid, name) VALUES (3, 'すーくん');
