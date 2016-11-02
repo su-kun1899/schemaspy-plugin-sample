c383044e4355af41f672628b29f053a995e13e37
su-kun1899 <higedrum.coz@gmail.com>
2016-11-02 20:19:23 +0900

    Add books

---
+++ src/main/resources/db/migration/V1_3_2__add_books.sql
@@ -0,0 +1,5 @@
+INSERT INTO book (isbn, title, publisherid, author) VALUES (10, 'ふーたくの冒険', 1, 'ふーたくん');
+INSERT INTO book (isbn, title, publisherid, author) VALUES (20, 'ふーたくの伝説', 1, 'ふーたくん');
+INSERT INTO book (isbn, title, publisherid, author) VALUES (30, 'ひさっしゅの冒険', 2, 'いまむー');
+INSERT INTO book (isbn, title, publisherid, author) VALUES (40, 'ひさっしゅの伝説', 2, 'いまむー');
+INSERT INTO book (isbn, title, publisherid, author) VALUES (50, 'すーくんの冒険', 3, 'すどぅー');
