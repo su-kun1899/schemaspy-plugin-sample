55748d25cf4a2304ee6e08c4af8472c8bf6b4397
su-kun1899 <higedrum.coz@gmail.com>
2016-11-11 12:21:52 +0900

    Add tablename loop

--- .script/dmp.sh
+++ .script/dmp.sh
@@ -4,9 +4,16 @@ readonly DB_HOST='localhost'
 readonly DATABASE='sample'
 readonly SCRIPT_DIR=$(cd $(dirname $0);pwd)
 readonly DML_DIR="${SCRIPT_DIR}/../src/main/resources/dml"
+readonly TARGET_TABLES=(
+book
+publisher
+)
 
-#TODO テーブル名のループにしたい
-mysqldump --user $DB_USER -h $DB_HOST --complete-insert --hex-blob --no-create-info --single-transaction $DATABASE book > ${DML_DIR}/book_dml.sql
-mysqldump --user $DB_USER -h $DB_HOST --complete-insert --hex-blob --no-create-info --single-transaction $DATABASE publisher > ${DML_DIR}/publisher_dml.sql
+#TODO テーブル名は外部ファイルから取得したい
+for tablename in ${TARGET_TABLES[@]}
+do
+  echo $tablename
+  mysqldump --user $DB_USER -h $DB_HOST --complete-insert --hex-blob --no-create-info --single-transaction $DATABASE $tablename > ${DML_DIR}/${tablename}_dml.sql
+done
 
 exit 0
